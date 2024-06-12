b946df41f28acab13dd635a69af4e7ba1a644873
Ranly1145 <roma.garbuz12@yandex.ru>
2024-06-12 18:28:11 +0300

    Twenty seventh commit

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -1,7 +1,10 @@
 # flake8: noqa
 import os
 from django.core.management.utils import get_random_secret_key
+from dotenv import load_dotenv
 from pathlib import Path
+
+load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
